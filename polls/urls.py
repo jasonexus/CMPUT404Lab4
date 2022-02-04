d829f64ba0f0da9937af47961b7fa89a48fc0267
jasonexus <jasonexus7458@gmail.com>
2022-02-04 15:48:14 -0700

    Added serializer

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
+    path('api/question/<int:pk>', views.update_question, name='update_question'),
+    path('api/questions/', views.get_questions, name='get_questions'),
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
